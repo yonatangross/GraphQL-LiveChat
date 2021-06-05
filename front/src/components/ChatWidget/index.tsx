b4d482d59a6fdc1b705dc64582e85a3b062c963f
yonatangross <yonatan2gross@gmail.com>
2021-06-05 15:18:27 +0300

    init project and front

---
+++ front/src/components/ChatWidget/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles.css'
+interface Props {
+    
+}
+
+const ChatWidget = (props: Props) => {
+    return (
+        <div className="widgetContainer">
+            
+        </div>
+    )
+}
+
+export default ChatWidget
---
+++ front/src/components/ConversationList/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles.css'
+interface Props {
+    
+}
+
+const ConversationList = (props: Props) => {
+    return (
+        <div className="widgetContainer">
+            
+        </div>
+    )
+}
+
+export default ConversationList
